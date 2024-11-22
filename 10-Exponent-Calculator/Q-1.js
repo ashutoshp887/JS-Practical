8eee4d300e613ba0e9ef55ff3eb82c01638801c0
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-23 01:55:42 +0530

    new folder

--- 10-Exponent-Calculator/Q-1.js
+++ 10-Exponent-Calculator/Q-1.js
@@ -1,1 +1,11 @@
-f
+function ashu(input,input3){
+    if(typeof input3 !== "number"){
+        throw new Error("should be in number")
+    }
+ let result =1
+for(i=1;i<=input3;i++){
+   result = result * input
+}
+return result
+}
+console.log(ashu(5,3))
