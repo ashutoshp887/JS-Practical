18984b3545efab96b593b416c0122eb3ea32df5d
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-23 01:58:55 +0530

    new folder

--- 10-Exponent-Calculator/Q-1.js
+++ 10-Exponent-Calculator/Q-1.js
@@ -1,3 +1,14 @@
+/**
+ * 
+ * Write a function that returns the result of raising a givem number to a specified power
+ * Input: Two numbers
+ * Base
+ * Exponent
+ * Return: Base to the Power of exponent 
+ */
+
+
+
 function ashu(input,input3){
     if(typeof input3 !== "number"){
         throw new Error("should be in number")
