7da71f2ebcd764d3d372a71bf994b8d9354b737a
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-23 01:26:12 +0530

    new

--- 9-Check-Palindromes/Q-1.js
+++ 9-Check-Palindromes/Q-1.js
@@ -25,4 +25,40 @@ return true
 }
 
 console.log(isPalindromes("madami"))
- h
+ 
+
+
+ // 2nd Method
+
+ function ashu(input){
+input = input.toLowerCase();
+let start =0;
+let end = input.lenghth-1
+for(start,end; start<=end;start++,end--){
+    if(input[start]!==input[end]){
+        return false;
+    }
+    
+}
+return true;
+ }
+
+ console.log(ashu("Madam"))
+
+
+ //3rd Method
+
+ function ashu1(input1){
+const split= input1.split('');
+
+const reverse = split.reverse()
+
+const join= reverse.join('')
+
+if(input1 === join){
+    return true;
+}
+return false;
+ }
+
+ console.log(ashu1("madam"))
